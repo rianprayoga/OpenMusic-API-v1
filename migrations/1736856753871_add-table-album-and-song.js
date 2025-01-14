7bc6f4ddcc19a48b2a9e18543e59dad24270fc05
rian <junianto.riansp@gmail.com>
2025-01-14 20:07:39 +0700

    chore: add migration for albums and songs

---
+++ migrations/1736856753871_add-table-album-and-song.js
@@ -0,0 +1,37 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('albums', {
+        id: {type: 'varchar(16)', primaryKey: true, notNull: true },
+        name: { type: 'varchar(100)', notNull: true },
+        year: { type: 'integer', notNull: true },
+      });
+
+      pgm.createTable('songs', {
+        id: {type: 'varchar(16)', primaryKey: true, notNull: true },
+        title: { type: 'varchar(100)', notNull: true },
+        year: { type: 'integer', notNull: true },
+        genre: { type: 'varchar(100)', notNull: true },
+        performer: { type: 'varchar(100)', notNull: true },
+        duration: { type: 'integer', notNull: false },
+        albumid: { type: 'varchar(16)', notNull: false, references: '"albums"' },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('songs');
+    pgm.dropTable('albums');
+};
